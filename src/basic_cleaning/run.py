5740cdb6e75ea2ed0d8bcfed356f35023eb32d19
Fadhel610 <fadhel.h26@gmail.com>
2022-02-26 22:10:57 +0300

    Adding a code that will remove rows that are not in NYC

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -30,6 +30,10 @@ def go(args):
     df = df[idx].copy()
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    logger.info("Dropping rows that are not in NYC")
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+    
     logger.info(f'Saving the cleaned dataframe as csv')
     df.to_csv('clean_sample.csv', index = False)
 
